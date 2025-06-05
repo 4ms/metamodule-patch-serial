dd5ca4589e8e92c5512dfbb92bb186246afbbc9f
Dan Green <danngreen1@gmail.com>
2025-06-05 09:06:57 -0700

    yaml parsing errors are not printed to console by default (only if LOG_LEVEL >= INFO)

--- ryml/ryml_init.cc
+++ ryml/ryml_init.cc
@@ -3,6 +3,13 @@
 
 namespace RymlInit
 {
+
+#if defined(LOG_LEVEL) && (LOG_LEVEL > 2)
+#define ryml_printf printf
+#else
+#define ryml_printf(...)
+#endif
+
 void init_once() {
 	static bool already_init = false;
 	static c4::yml::Callbacks callbacks;
@@ -10,9 +17,9 @@ void init_once() {
 		already_init = true;
 		callbacks.m_error = [](const char *msg, size_t /*msg_len*/, c4::yml::Location loc, void * /*user_data*/) {
 			if (loc.name.empty())
-				printf("[ryml] %s\n", msg);
+				ryml_printf("[ryml] %s\n", msg);
 			else
-				printf("[ryml] %s in %s %zu:%zu)\n", msg, loc.name.data(), loc.line, loc.col);
+				ryml_printf("[ryml] %s in %s %zu:%zu)\n", msg, loc.name.data(), loc.line, loc.col);
 		};
 		c4::yml::set_callbacks(callbacks);
 		c4::set_error_flags(c4::ON_ERROR_DEBUGBREAK | c4::ON_ERROR_LOG);
