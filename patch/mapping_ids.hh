b6cec013f593d9729f6608cbbe146ac637e1fc72
Dan Green <danngreen1@gmail.com>
2025-06-25 21:15:31 -0700

    More constants regarding button expanders

--- patch/mapping_ids.hh
+++ patch/mapping_ids.hh
@@ -4,6 +4,9 @@ static constexpr auto MaxPanelKnobs = 12;
 static constexpr auto MaxExpButtons = 32;
 static constexpr auto MaxExpPots = 32;
 
+static constexpr auto ButtonsPerExpander = 8;
+static constexpr auto MaxButtonExpanders = MaxExpButtons / ButtonsPerExpander;
+
 enum {
 	FirstExpKnob = MaxPanelKnobs,					   //12
 	LastPossibleKnob = MaxPanelKnobs + MaxExpPots - 1, //43
